1be4d41541b72513adb65d4c23900e778650cefb
dtechshoemaker <peterkimbeghi@gmail.com>
2025-07-15 22:55:29 +0100

    Added the logo tag

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=400)
+    picture = models.ImageField()
+
+    def __str__(self):
+        return self.user.username
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        user_form = CreateUserForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('login')
+    else:
+        user_form = CreateUserForm()
+
+    context = {
+        'user_form': user_form
+    }
+
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    return render(request, 'login.html')
+
+
