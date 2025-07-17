2a4c662b0c970de9d943b2669e2b6da44119cff7
dtechshoemaker <peterkimbeghi@gmail.com>
2025-07-17 03:59:23 +0100

    update: Added widget to the forms.py and displayed the form in template

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,40 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import User
+from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control my-2 py-2',
+            'placeholder': 'Username here ...'
+        })
+    )
+
+    email = forms.EmailField(
+        label="Email",
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email here ...'
+        })
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password'
+        })
+    )
+
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm Password'
+        })
+    )
 
     class Meta:
         model = User
